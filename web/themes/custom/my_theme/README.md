af5d977a6f11982130fd3c2e39a9db0a20050f11
Mina Bui <mina@affinitybridge.com>
2023-02-21 16:06:06 -0800

    Initial commit using D10 site template and starter custom theme.

---
+++ web/themes/custom/my_theme/README.md
@@ -0,0 +1,44 @@
+# nofrills
+
+nofrills is a clean **Drupal 8/9 starter theme** (or boilerplate) with Webpack, Tailwind CSS, SASS (+ Autoprefixer), Babel (ES6), Uglify, Imagemin and more.
+
+### Features
+
+- Base theme is stable so you have bare **minimum of classes** and wrapping divs in your HTML
+- **Webpack** powered to transform and watch **Sass** (with Autoprefixer), **JavaScript** (ES6 with Babel) and **images** (imagemin)
+- Tailwind CSS as a utility-first CSS framework for rapidly building custom designs [Learn more](https://tailwindcss.com/)
+
+### Requirements
+
+- Node (> 10.7)
+- NPM or Yarn (> 6.4)
+
+### Installation
+
+1. Go to your themes folder and clone this repo into it:
+
+```bash
+$ cd web/themes/ && git clone git@github.com:FNGR2911/nofrills.git YOUR_THEME_NAME
+```
+
+2. Install all dependencies
+
+```bash
+$ npm install
+```
+
+3. Run the dev script and start developing
+
+```bash
+$ npm run dev
+```
+
+4. To get production optimized assets, run build
+
+```bash
+$ npm run build
+```
+
+### License
+
+MIT
---
+++ web/themes/custom/my_theme/config/webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, '../assets'),
+    filename: 'js/bundle.js',
+  },
+  stats: {
+    colors: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { context: 'src', from: 'images/**/*.+(jpg|jpeg|png|gif|svg)' },
+        { context: 'src', from: 'fonts/*.+(woff|woff2|otf|eot|svg)' },
+      ],
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      plugins: [imageminMozjpeg({ quality: 80, progressive: true })],
+    }),
+  ],
+}
